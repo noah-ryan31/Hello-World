c1eb1654f91af448dcd0cf1f398ad7717b129486
noah-ryan31 <noahryan1998@gmail.com>
2017-01-24 14:23:48 -0800

    Update new.java

--- new.java
+++ new.java
@@ -10,7 +10,16 @@
 
   <!-- The "body" is the part of your document where you include all content you want to show up in the browser -->
  <body>
-  <p>hello DVC</p>
+  window.onscroll = function() {
+  var el = document.getElementsByClassName('header')[0];
+  var className = 'small';
+  if (el.classList) {
+    if (window.scrollY > 10)
+      el.classList.add(className);
+    else
+      el.classList.remove(className);
+  }
+};
  </body>
    
 </html> 
